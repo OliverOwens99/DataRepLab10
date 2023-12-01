875757fa20bccee0852af2c32cc575d487475e6e
Oliver <g00409776@atu.ie>
2023-12-01 09:54:27 +0000

    added delete handling client side

--- src/my_components/bookItem.js
+++ src/my_components/bookItem.js
@@ -1,20 +1,29 @@
 import { CardBody, CardImg, CardTitle } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 // function to represent a single book item
 function BookItem(props) {
 
     return (
         // create a card to display the data to the screen
-            <Card style={{ width: '18rem' }}>   
+        <Card style={{ width: '18rem' }}>
             <CardBody>
-            {/* this is to display data to the screen */}
-            {console.log(props.myBooks.title)}
-            <CardTitle> Title: {props.myBooks.title}</CardTitle> 
-            <CardTitle>Authors: {props.myBooks.authors}</CardTitle>
-            <Card.Img  variant = "top" src={props.myBooks.cover}></Card.Img>
+                {/* this is to display data to the screen */}
+                {console.log(props.myBooks.title)}
+                <CardTitle> Title: {props.myBooks.title}</CardTitle>
+                <CardTitle>Authors: {props.myBooks.authors}</CardTitle>
+                <Card.Img variant="top" src={props.myBooks.cover}></Card.Img>
             </CardBody>
-            <Link to={"/edit/" +props.myBooks._id} className='btn btn_primary'>Edit</Link>
+            <Link to={"/edit/" + props.myBooks._id} className='btn btn-primary'>Edit</Link>
+            <Button variant="danger" onClick={
+                (e) => {
+                    axios.delete('http://localhost:4000/api/books/' + props.myBooks._id)
+                    .then()
+                    .catch();
+                }
+            }>Delete</Button>
         </Card>
     );
 }
